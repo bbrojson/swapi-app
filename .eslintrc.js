2e5ef7380e9a3c8bfc25b530f1f5b35da670012e
bbrojson <blazej.brojson@gmail.com>
2020-10-25 08:14:05 +0100

    init commit

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: [
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    //'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  rules: {
+    "linebreak-style": 0,
+    // "arrow-body-style": 1,
+    // "no-shadow": 1,
+    // "arrow-parens": [0, "as-needed"],
+
+    // "@typescript-eslint/comma-dangle": 0,
+    // "@typescript-eslint/no-loop-func": 0,
+    // "@typescript-eslint/no-redeclare": 0,
+    // "@typescript-eslint/no-shadow": 0,
+    // "@typescript-eslint/dot-notation": 0,
+  }
+};
---
+++ TODO.md
@@ -0,0 +1,9 @@
+# TODO
+ - add dependencies
+ - config ESLint
+ - i18next
+ - add Material-UI
+ - api fetch module
+ - redux
+ - UI and styles
+ - tests
