2b00b77d0f5af01028d325c0b61864bafe797917
bbrojson <blazej.brojson@gmail.com>
2020-11-11 10:53:03 +0100

    style: Add header styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,46 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { makeStyles } from '@material-ui/core/styles';
 import {
-  Link,
+  Typography,
+  Button,
+  Divider,
+  Container,
+  Grid,
+} from '@material-ui/core';
+import {
+  Link as RouterLink,
 } from 'react-router-dom';
+
+const useStyles = makeStyles(() => ({
+  starJediFont: {
+    fontFamily: 'Star Jedi',
+  },
+  logoLink: {
+    display: 'block',
+    color: '#fff',
+  },
+}));
 
 export default function Header() {
   const { t } = useTranslation();
+  const classes = useStyles();
   return (
     <header>
-      <Link to="list">
-        {t('SW APi')}
-      </Link>
+      <Container maxWidth="lg">
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <Button className={classes.logoLink} color="primary" component={RouterLink} to="/list">
+              <Typography align="center" component="h1" variant="h3" className={classes.starJediFont}>
+                {t('SW APJ  $ #')}
+              </Typography>
+            </Button>
+          </Grid>
+          <Grid item xs={12}>
+            <Divider light />
+          </Grid>
+        </Grid>
+      </Container>
     </header>
   );
 }
